aa1a8b9243d7e7906861551df1b240abb86f60fa
Asgard\Filip <f.kralik96@gmail.com>
2023-04-23 16:54:13 +0200

    style: login and otp

--- accounts/forms.py
+++ accounts/forms.py
@@ -150,10 +150,6 @@ class LoanForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs.update({
                 'class': (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 '
-                    'rounded py-3 px-4 leading-tight '
-                    'focus:outline-none focus:bg-white '
-                    'focus:border-gray-500'
+                    'py-2 px-3 pr-11 block w-full border border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400'
                 )
             })
