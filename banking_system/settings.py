7f6d26659898c72fecde343996f3d31912ea20e8
Brendan Shelley <brendan@shellshock.net.au>
2023-11-06 23:15:02 +1100

    rest test

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -183,3 +183,11 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': 10800.0,
     },
 }
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
