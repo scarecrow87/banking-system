8473944ebbe506d8efe44b208c38f0481f8355fd
Brendan Shelley <brendan@shellshock.net.au>
2023-11-19 22:30:14 +1100

    api2

--- ui/urls.py
+++ ui/urls.py
@@ -6,6 +6,11 @@ from rest_framework import routers, serializers, viewsets
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
 app_name = 'ui'
 
 urlpatterns = [
