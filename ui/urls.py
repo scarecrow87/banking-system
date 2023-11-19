8bcbcb24735d4038fd860586dd51394039b268b2
Brendan Shelley <brendan@shellshock.net.au>
2023-11-19 22:33:44 +1100

    undo

--- ui/urls.py
+++ ui/urls.py
@@ -2,19 +2,10 @@ from django.urls import path
 
 from accounts.views import UserRegistrationView, LogoutView, UserLoginView, UserAccountView, \
     UserSavingAccountView, UserRegistrationSavingAccountView, UserLoanView, get_account_type_description, UserDetailView
-from rest_framework import routers, serializers, viewsets
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 app_name = 'ui'
 
 urlpatterns = [
    path('details', UserDetailView.as_view(), name="edit_user"),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
- 
+
 ]
