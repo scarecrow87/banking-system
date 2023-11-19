c24057d306bba1d4dd3d7d0a183e6ba95999029b
Brendan Shelley <brendan@shellshock.net.au>
2023-11-19 22:29:03 +1100

    api test

--- ui/urls.py
+++ ui/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from accounts.views import UserRegistrationView, LogoutView, UserLoginView, UserAccountView, \
     UserSavingAccountView, UserRegistrationSavingAccountView, UserLoanView, get_account_type_description, UserDetailView
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 app_name = 'ui'
 
 urlpatterns = [
-   path('details', UserDetailView.as_view(), name="edit_user")
+   path('details', UserDetailView.as_view(), name="edit_user"),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
  
 ]
